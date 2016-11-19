bf8604a5d175d04c561f831e4e561e4af0089b9a
Joe Maples <joe@frap129.org>
2016-11-19 11:45:28 -0500

    Update to 3.0: Add concept widgets, enable proguard
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ app/src/main/java/org/frap129/pillwidget/PillWidgetHollowProvider.java
@@ -0,0 +1,39 @@
+package org.frap129.pillwidget;
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class PillWidgetHollowProvider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        PackageManager pm = context.getPackageManager();
+        String packageName = "com.google.android.googlequicksearchbox";
+        for(int j = 0; j < appWidgetIds.length; j++)
+        {
+            int appWidgetId = appWidgetIds[j];
+
+            try {
+                Intent intent = pm.getLaunchIntentForPackage(packageName);
+                intent.addCategory("android.intent.category.LAUNCHER");
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        context, 0, intent, 0);
+                RemoteViews views = new RemoteViews(context.getPackageName(),
+                        R.layout.pill_hollow);
+                views.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(context.getApplicationContext(),
+                        "Please install the Google app",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
